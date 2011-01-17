e8754d4feb5844faab7a1f42c886be66e6d75207
Steve Hoeksema <steve@seven.net.nz>
2011-01-17 20:09:46 +1300

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synesthetic::Application.config.secret_token = '47837c0a30611382d9699ee1368b40544c45cd28aa09a67904330dc7e5446095f99b7eebcb133d1fdc41bfe85c9d5f66577134b14f8b613d2f0e9974c00ecdbe'
